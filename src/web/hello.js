2e37dae800be16ed459487ff584697dabe3db81e
Peter de Croos <cultofmetatron@aumlogic.com>
2015-01-27 16:12:49 -0800

    updating dependencies

--- src/web/hello.js
+++ src/web/hello.js
@@ -13,6 +13,7 @@ React.renderComponent(
 
 */
 
+/*
 var Engine = require('famous/core/Engine');
 var Surface = require('famous/core/Surface');
 
@@ -31,4 +32,39 @@ var firstSurface = new Surface({
 firstSurface.setContent('<h1>HELLO WORLD, I am thine god on earth</h1>');
 
 mainContext.add(firstSurface);
+*/
+var Engine = require('famous/core/Engine');
+var Surface = require('famous/core/Surface');
+var Transform = require('famous/core/Transform');
+var StateModifier = require('famous/modifiers/StateModifier');
+var Transitionable = require('famous/transitions/Transitionable');
+var SpringTransition = require('famous/transitions/SpringTransition');
+Transitionable.registerMethod('spring', SpringTransition);
+
+var mainContext = Engine.createContext();
+
+var surface = new Surface({
+  size: [100, 100],
+  properties: {
+    color: 'white',
+    textAlign: 'center',
+    backgroundColor: '#FA5C4F'
+  }
+});
+
+var stateModifier = new StateModifier({
+  origin: [0.5, 0]
+});
+
+mainContext.add(stateModifier).add(surface);
+
+var spring = {
+  method: 'spring',
+  period: 1000,
+  dampingRatio: 0.3
+};
+
+stateModifier.setTransform(
+  Transform.translate(0, 300, 0), spring
+);
 
